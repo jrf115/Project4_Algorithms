7648f6b47a505915bc230e8f9b48aebbfa4e47f7
jrf115 <jrf115@zips.uakron.edu>
2019-11-21 21:25:35 -0500

    Create seam435.cpp

---
+++ seam435.cpp
@@ -0,0 +1,24 @@
+/*****
+ * John Fahringer
+ * Project 4 Algorihtms
+ * Seam Carving 
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+
+int main(int charc, char argv[])
+{
+	if (argc < 3)
+		cout << "Error: Running program format must be: ./seam435 something something" << endl;
+	else {
+
+	}
+}
