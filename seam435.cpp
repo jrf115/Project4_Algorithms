74550fddc9a0a3c24a49b34d5feedfe4f03bd6a5
jrf115 <jrf115@zips.uakron.edu>
2019-12-01 14:28:25 -0500

    Add new printArray(), char2PostInt, and implement build_energy_array()
    I also added a test matrix in main that represents the first test case in SeamCarvingCalculator.xlsx

--- seam435.cpp
+++ seam435.cpp
@@ -1,12 +1,13 @@
 /*****
  * John Fahringer
  * Project 4 Algorihtms
- * Seam Carving 
+ * Seam Carving
  */
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdlib.h> /* abs */
 
 
 using std::cout;
@@ -22,15 +23,47 @@ void carve_HorSeam(char* pgm_arr, int seams);
 void rotateArray90(int& columns, int& rows, char* pgm_arr);
 void rotateArray270(int& columns, int& rows, char* & pgm_arr);
 void printRawArray(int columns, int rows, char arr[]);
+void printArray(int columns, int rows, char arr[]);
+int char2PosInt(char c);
 
-
+/***
+Energy for a specifc pixel can be found using:
+	e(i, j) = | v(i, j) - v(i - 1, j) | + | v(i, j) - v(i + 1, j) | + | v(i, j) - v(i, j - 1) | + | v(i, j) - v(i, j + 1) | ,
+Where:
+	v(i, j) = pixel value at(i, j)
+*/
 char* build_Energy_Array(int columns, int rows, char pgm_arr[])
 {
 	char* energy = new char[columns * rows];
-	for (int p_row(0); p_row < rows; p_row++)
-	{
+	int energy_value(0);
+	for (int p_row(0); p_row < rows; p_row++) {
+		for (int p_column(0); p_column < columns; p_column++) {
+			// Checking if v(i - 1, j) lies outside of the visual matrix space or not
+			cout << p_column - 1 << endl;
+			if (p_column - 1 >= 0)
+				energy_value += abs(char2PosInt(pgm_arr[p_column + p_row * columns]) - char2PosInt(pgm_arr[p_column - 1 + p_row * columns]));
+			// Check if v(i + 1, j) is outside of the visual matrix space or not
+			cout << p_column + 1 << endl;
+			if (p_column + 1 < columns)
+				energy_value += abs(char2PosInt(pgm_arr[p_column + p_row * columns]) - char2PosInt(pgm_arr[p_column + 1 + p_row * columns]));
+			// Check if v(i, j - 1) is outside of the visual matrix space or not
+			cout << p_row - 1 << endl;
+			if (p_row - 1 >= 0)
+				energy_value += abs(char2PosInt(pgm_arr[p_column + p_row * columns]) - char2PosInt(pgm_arr[p_column + (p_row - 1) * columns]));
+			// Check if v(i, j + 1) is outside of the visual matrix space or not
+			cout << p_row + 1 << endl;
+			if (p_row + 1 < rows)
+				energy_value += abs(char2PosInt(pgm_arr[p_column + p_row * columns]) - char2PosInt(pgm_arr[p_column + (p_row + 1) * columns]));
+			cout << "energy_value: " << char2PosInt(energy_value) << endl;
+			cout << endl;
 
+			energy[p_column + p_row * columns] = energy_value;
+			energy_value = 0;
+		}
 	}
+	cout << "Energy Array Produced: \n";
+	printArray(columns, rows, energy);
+	return energy;
 }
 
 void rotateArray90(int& columns, int& rows, char* pgm_arr)
@@ -41,7 +74,7 @@ void rotateArray90(int& columns, int& rows, char* pgm_arr)
 		copy[i] = pgm_arr[i];
 
 	for (int c(0); c < columns; c++) {
-		for (int r(rows - 1); r >= 0; r--) {	
+		for (int r(rows - 1); r >= 0; r--) {
 			pgm_arr[rotated_c + rotated_r * rows] = copy[c + r * columns];
 			//cout << "copied " << c << " + " << r << " * " << columns << " == " << c + r * columns << " _____TO_____ " << rotated_c << " + " << rotated_r << " * " << rows << " == " << rotated_c + rotated_r * rows << endl;
 			rotated_c++;
@@ -49,7 +82,7 @@ void rotateArray90(int& columns, int& rows, char* pgm_arr)
 		rotated_r++;
 		rotated_c = 0;
 	}
-	
+
 	// Delete old array and switch columns/rows
 	delete copy;
 	int temp(columns);
@@ -75,7 +108,7 @@ void rotateArray270(int& columns, int& rows, char* pgm_arr) {
 /* Prints the stored char value as an int. May print out 2's compliments of numbers */
 void printRawArray(int columns, int rows, char arr[])
 {
-	cout << endl << "Reading from 2dim_char array: " << endl;
+	cout << endl << "Reading from raw 2dim_char array: " << endl;
 	cout << "Columns: " << columns << " and rows: " << rows << endl;
 	for (int r(0); r < rows; r++) {
 		for (int c(0); c < columns; c++)
@@ -84,6 +117,24 @@ void printRawArray(int columns, int rows, char arr[])
 	}
 }
 
+void printArray(int columns, int rows, char arr[])
+{
+	cout << "Columns: " << columns << " and rows: " << rows << endl;
+	for (int r(0); r < rows; r++) {
+		for (int c(0); c < columns; c++) {
+				cout << char2PosInt(arr[columns * r + c]) << " "; // The current value is a 2's compliment.
+		}
+		cout << endl;
+	}
+}
+
+int char2PosInt(char c)
+{
+	if (int(c) < 0)
+		return 256 + int(c); // The current value is a 2's compliment.
+	else
+		return int(c);
+}
 
 int main(int argc, char *argv[])
 {
@@ -128,14 +179,21 @@ int main(int argc, char *argv[])
 			cout << endl;
 		}
 		/*////////////////////////////////////////////////////////////////////////////////////////////
-		// We can see some of the int values, that are above 127, get stored into the char array as 2's compliments. 
+		// We can see some of the int values, that are above 127, get stored into the char array as 2's compliments.
 		// Take this into account when we have to use the non 2's compliment forms of these numbers.
-		
+
 		printRawArray(columns, rows, pgm_Arr);
 		rotateArray90(columns, rows, pgm_Arr);
 		cout << "RotatedArray" << endl;
 		printRawArray(columns, rows, pgm_Arr);
 		/////////////////////////////////////////////////////////////////////////////////////////////*/
+		char testArray[24]; testArray[0] = (30);	testArray[1] = (120);	testArray[2] = (196); testArray[3] = (215);	testArray[4] = (198); testArray[5] = (180); testArray[6] = (45);	testArray[7] = (4);	testArray[8] = (172); testArray[9] = (5); testArray[10] = (250);	testArray[11] = (252); testArray[12] = (190); testArray[13] = (192);	testArray[14] = (53); testArray[15] = (35); testArray[16] = (123); testArray[17] = (124); testArray[18] = (5); testArray[19] = (100); testArray[20] = (132); testArray[21] = (110);	testArray[22] = (114); testArray[23] = (203);
+		int roww = 8;
+		int coll = 3;
+		printArray(coll, roww, testArray);
+		printRawArray(coll, roww, testArray);
+		char* nrg_Arr = build_Energy_Array(coll, roww, testArray);
+		// char* nrg_Arr = build_Energy_Array(columns, rows, pgm_Arr);
 
 		// Testing outputfile...
 		outputFile.open("testingoutput.pgm");
